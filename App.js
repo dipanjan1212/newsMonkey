58349f0bf7ad452bf3e706d0480b2fe07f634e4c
Dipanjan Das <dipanjanrick2002@gmail.com>
2023-01-06 18:33:58 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,34 @@
+import './App.css';
+
+import React, { Component } from 'react'
+import NavBar from './components/Navbar';
+import News from './components/News';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+
+export default class App extends Component {
+  pageSize  = 5;
+  apiKey = process.env.REACT_APP_NEWS_API
+  render() {
+    return (
+      <div>
+        <Router>
+        <NavBar/> 
+        <Switch>
+          <Route exact path="/"><News key="general" pageSize ={this.pageSize } apiKey={this.apiKey}country="in" category="general"/></Route> 
+          <Route exact path="/business"><News key="business" pageSize ={this.pageSize } apiKey={this.apiKey}country="in" category="business"/></Route> 
+          <Route exact path="/entertainment"><News key="entertainment" pageSize ={this.pageSize }apiKey={this.apiKey} country="in" category="entertainment"/></Route> 
+          <Route exact path="/general"><News key="general" pageSize ={this.pageSize }apiKey={this.apiKey} country="in" category="general"/></Route> 
+          <Route exact path="/health"><News key="health" pageSize ={this.pageSize }apiKey={this.apiKey} country="in" category="health"/></Route> 
+          <Route exact path="/science"><News key="science" pageSize ={this.pageSize }apiKey={this.apiKey} country="in" category="science"/></Route> 
+          <Route exact path="/sports"><News key="sports" pageSize ={this.pageSize } apiKey={this.apiKey}country="in" category="sports"/></Route> 
+          <Route exact path="/technology"><News key="technology" pageSize ={this.pageSize }apiKey={this.apiKey} country="in" category="technology"/></Route> 
+        </Switch>
+        </Router>
+      </div>
+    )
+  }
+}
